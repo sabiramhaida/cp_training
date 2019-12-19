a5bf6f6273c29e0582098ee77c83a49531b3f192
Sabir Amhaida <sabir.amhaida@um5s.net.ma>
2019-12-19 02:53:32 +0100

    Delete That Is My Score.py

--- December challenge 2019 div 2/That Is My Score.py
+++
@@ -1,8 +0,0 @@
-for t in range(int(input())):
-    scores = [0,0,0,0,0,0,0,0]
-    n=int(input())
-    for i in range(n):
-        p, sc = list(map(int,input().split()))
-        if(p <= 8):
-            scores[p-1] = max(scores [p-1],sc)
-    print(sum(scores))
